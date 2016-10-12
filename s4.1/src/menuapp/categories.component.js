fe7f8921b1a244a15b32faa04ad3613fa46c14b4
bach <bach@BACH-MAC>
2016-10-12 23:21:54 +0200

    added search

--- s4.1/src/menuapp/categories.component.js
+++ s4.1/src/menuapp/categories.component.js
@@ -9,9 +9,23 @@
         })
         .controller('ComponentCategoriesController', ComponentCategoriesController);
 
-    ComponentCategoriesController.$inject = ['$http', '$mdToast', 'MenuDataService'];
-    function ComponentCategoriesController($http, $mdToast, MenuDataService) {
+    ComponentCategoriesController.$inject = ['$http', '$mdToast', 'MenuDataService', '$scope'];
+    function ComponentCategoriesController($http, $mdToast, MenuDataService, $scope) {
         this.paginatorCallback = MenuDataService.getAll;
+
+        this.getLoadResultsCallback = getLoadResultsCallback;
+
+        var loadPageCallbackWithDebounce;
+
+        $scope.$watch('filterText', function(){
+            if(loadPageCallbackWithDebounce){
+              loadPageCallbackWithDebounce();  
+            }
+        });
+
+        function getLoadResultsCallback(loadPageCallback){
+            loadPageCallbackWithDebounce = _.debounce(loadPageCallback, 1000);
+        }
     };
 
 })();
--- s4.1/src/menuapp/menudata.service.js
+++ s4.1/src/menuapp/menudata.service.js
@@ -36,15 +36,16 @@
             return response;
         };
 
-        service.getAll = function (page, pageSize) {
-            var offset = (page - 1) * pageSize;
+        service.getAll = function (page, pageSize, filterText) {
+            var offset = (page-1) * pageSize;
+            var query = filterText ? filterText : '';
 
             return $http.post('https://api.nutritionix.com/v1_1/search', {
                 'appId': 'a03ba45f',
                 'appKey': 'b4c78c1472425c13f9ce0e5e45aa1e16',
                 'offset': offset,
                 'limit': pageSize,
-                'query': '*',
+                'query': query + '*',
                 'fields': ['*'],
                 'sort': {
                     'field': 'nf_iron_dv',
